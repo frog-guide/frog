1b9df8723c0462a044e9c313d69db9154dd6c239
Keppelen <keppelen.as@gmail.com>
2013-02-04 21:05:10 -0200

    pasta new, botão de login, link na logo para home

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'html');
+  app.set('view engine', 'jade');
   app.set('view options', {
     layout: false
   });
